0de97b737b8561d8d705b5ff2492b661fceaf2cf
zcong1993 <1005997919@qq.com>
2019-01-07 15:46:34 +0800

    use dataloader

---
+++ src/dataloader/user.ts
@@ -0,0 +1,9 @@
+import * as Dataloader from 'dataloader'
+import axios from 'axios'
+
+const getUsers = (ids: any[]): Promise<any[]> => {
+  return axios.get<any[]>(`http://localhost:8080/v1/users_batch?ids=${ids.join(',')}`)
+    .then(res => res.data)
+}
+
+export const userLoader = new Dataloader<any, any[]>(getUsers)
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { ApolloServer, gql } from 'apollo-server'
 import { RedisCache } from 'apollo-server-cache-redis'
 import { ReadonlyDataSource } from '@zcong/apollo-datasource-rest-plus'
 import { UserDatasource, MeDatasource } from './datasource'
+import { userLoader } from './dataloader/user'
 
 const typeDefs = gql`
   # Comments in GraphQL are defined with the hash (#) symbol.
@@ -17,15 +18,19 @@ const typeDefs = gql`
     email: String
   }
 
-  type BookList {
+  type BookList implements PagedData {
     totalCount: Int!
     data: [Book]!
+  }
+
+  interface PagedData {
+    totalCount: Int!
   }
 
   # The "Query" type is the root of all GraphQL queries.
   # (A "Mutation" type will be covered later on.)
   type Query {
-    books(limit: Int!, offset: Int!): BookList!
+    books(limit: Int!, offset: Int!): PagedData!
     getBook(id: Int!): Book!
     me: User!
   }
@@ -49,7 +54,17 @@ const resolvers = {
   },
   Book: {
     author: async (_source: any, args: any, { dataSources }: any) => {
-      return dataSources.usersApi.getUser(_source.author_id)
+      // return dataSources.usersApi.getUser(_source.author_id)
+      return userLoader.load(_source.author_id)
+    }
+  },
+  PagedData: {
+    __resolveType(data: any, context: any, info: any) {
+      if (data.data[0].title) {
+        // console.log(info.schema)
+        return info.schema.getType('BookList')
+      }
+      return null
     }
   }
 }
